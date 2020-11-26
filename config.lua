edf110a33f2e816d9bf4460c17e6973c4f543f49
Maniusdezeb <73640789+Maniusdezeb@users.noreply.github.com>
2020-11-26 22:31:45 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,121 @@
+MN = {}
+
+-- Script 100% Custom made by Manius#0810 -- 
+
+-- I know Config is beetje rommelig maar je komt er wel aan uit :P
+-- IN toekomstige script is dit allemaal clean hou me Github in de gaten :)
+
+MN.drawinteract = 20
+MN.openmenu = 168
+
+MN.Startcar = 'sultanrs'
+MN.boot = 'seashark'
+MN.escapevoertuig = 'pony2'
+
+MN.drugstart = { 
+    ['1'] = {
+        menu = { 
+            x = 1092.84,
+            y = -2251.84,
+            z = 31.23,
+        }
+    }
+}    
+
+
+MN.marker  = {
+    r = 255,
+    g = 111,
+    b = 0,
+    size = {x = 0.3, y = 0.2, z = 0.1}
+}
+
+MN.startvoertuigspawn = { 
+    voertuig = { 
+        x = 1099.1, -- x COORDS
+        y = -2253.87, -- y COORDS
+        z = 30.24, -- z COORDS
+        h = 176.26, -- HEADING COORDS
+    },
+}
+
+MN.escapevoertuigspawn = { 
+    voertuig = { 
+        x = 1346.97, -- x COORDS
+        y = 4333.85, -- y COORDS
+        z = 38.13, -- z COORDS
+        h = 272.87, -- HEADING COORDS
+    },
+}  
+MN.bootvoertuig = { 
+    voertuig = { 
+        x = -725.79, -- x COORDS
+        y = -1328.2, -- y COORDS
+        z = 0.66, -- z COORDS
+        h = 227.52, -- HEADING COORDS
+    },
+} 
+
+
+MN.dropoff = { 
+    ['1'] = {
+        menu = { 
+            x = -740.97,
+            y = -1308.91,
+            z = 5,
+        }
+    }
+} 
+
+
+MN.finalloca = {
+    ['1'] = {
+        menu = { 
+            x = 1256.07,
+            y = 4288.27,
+            z = 30.8,
+        }
+
+    }
+}
+
+MN.pickup = {
+    ['1'] = {
+        menu = { 
+            x = 1308.93,
+            y = 4362.11,
+            z = 41.55,
+        }
+    }
+}
+
+MN.location = {
+    npc = {
+        x = 1350.34,
+        y = 4315.12,
+        z = 38.4,
+    },
+    pickup = {
+        x = 1308.93,
+        y = 4362.11,
+        z = 41.55,
+    },
+    finalloca = {
+        x = 1256.07,
+        y = 4288.27,
+        z = 30.8,
+    },
+    dropoff = {
+        x = -720.69,
+        y = -1325.06,
+        z = 1.6,
+    },
+    drugwaggie = {
+        x = 227.23,
+        y = 378.52,
+        z = 106.11,
+    },
+}
+
+
+
